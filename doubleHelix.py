2322b9ac31132094ec06458a2c3f873727539d2a
Tefo-K <tefokeabetswe4@gmail.com>
2023-08-10 13:04:20 +0100

    feat: created a function to generate a random name

--- doubleHelix.py
+++ doubleHelix.py
@@ -1,3 +1,11 @@
 import bpy
+import string
+import random
+
 bpy.ops.mesh.primitive_cube_add(size=1, enter_editmode=False, align='WORLD', location=(0, 0,
 0), scale=(1, 1, 1))
+
+#Random name generator
+def generate_random_name():
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for _ in range(5)) + ':' + str(random.randint(100, 999))
